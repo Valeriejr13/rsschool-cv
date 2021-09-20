dc4883dab7f8e87131c26f60e15054a7476ef388
Valeria Pinzon <valeriejr13@gmail.com>
2021-09-20 20:34:01 +0700

    feat: add html, css, js files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 /*Nav toggle*/
-document.querySelector('.nav-toggle').addEventListener('click', function(){
-    document.querySelector('.nav-toggle').classList.toggle('active');
-    document.querySelector('.nav_list').classList.toggle('active');
-});
+// document.querySelector('.nav-toggle').addEventListener('click', function(){
+//     document.querySelector('.nav-toggle').classList.toggle('active');
+//     document.querySelector('.nav_list').classList.toggle('active');
+// });
